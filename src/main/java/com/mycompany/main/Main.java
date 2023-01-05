a2aff6ee7afabaff79fb1108f275e63851a70d2c
Joao Vitor batistella <joaovitor.batistella.ifrs@gmail.com>
2023-01-05 19:27:02 -0300

    [serial]-05012023-1927

--- src/main/java/com/mycompany/main/Main.java
+++ src/main/java/com/mycompany/main/Main.java
@@ -3,15 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package com.mycompany.mavenproject1;
+package com.mycompany.main;
+
+import com.mycompany.main.views.MainForm;
 
 /**
  *
  * @author labhardware01
  */
-public class Mavenproject1 {
+public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        MainForm mainForm = new MainForm();
+        mainForm.setVisible(true);
     }
 }
